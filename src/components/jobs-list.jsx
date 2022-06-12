4153f0e923ed512bbfb1352a7b83baf976a24de2
Opa Kholis Majid <code@opakholis.space>
2022-06-12 07:46:08 +0700

    feat: add skeleton when data isn't fully loaded

--- src/components/jobs-list.jsx
+++ src/components/jobs-list.jsx
@@ -25,7 +25,21 @@ export const JobsList = () => {
       className="mx-auto mb-12 mt-28 w-full max-w-screen-lg scroll-mt-32 px-6 md:px-12"
     >
       <div className="md:masonry-2-col lg:masonry-3-col">
-        {jobs && jobs.data.map((job) => <Job key={job.id} data={job} />)}
+        {!jobs
+          ? Array.from({ length: 3 }).map((_, i) => <Skeleton key={i} />)
+          : jobs.data.map((job) => <Job key={job.id} data={job} />)}
+      </div>
+    </div>
+  );
+};
+
+const Skeleton = () => {
+  return (
+    <div className="mb-4 break-inside-avoid bg-white p-4 shadow-lg shadow-zinc-100 ">
+      <div className="animate-pulse">
+        <div className="h-10 w-full rounded bg-zinc-100" />
+        <div className="mt-4 mb-2 h-28 w-full rounded bg-zinc-100" />
+        <div className="h-8 w-full rounded bg-zinc-100" />
       </div>
     </div>
   );
