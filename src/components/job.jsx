962ad999d1574286141fba0ab6ec9d0cb820934d
Opa Kholis Majid <code@opakholis.space>
2022-06-12 10:01:58 +0700

    refactor: decrease font size and animation on hover

--- src/components/job.jsx
+++ src/components/job.jsx
@@ -2,10 +2,10 @@ export const Job = ({ data }) => {
   return (
     <a
       href=""
-      className="group mb-4 flex transform break-inside-avoid flex-col justify-between rounded-xl border border-zinc-100 bg-white p-4 shadow-lg shadow-zinc-100 transition duration-200 hover:scale-105 hover:border-indigo-400 hover:shadow-lg md:hover:scale-110"
+      className="group mb-4 flex transform break-inside-avoid flex-col justify-between rounded-xl border border-zinc-100 bg-white p-4 shadow-lg shadow-zinc-100 transition duration-200 hover:scale-105 hover:border-indigo-400 hover:shadow-lg lg:hover:scale-110"
     >
       <div>
-        <h5 className="text-lg font-bold text-indigo-600 md:text-2xl">
+        <h5 className="text-lg font-bold text-indigo-600 md:text-xl lg:text-2xl">
           {data.company_name}
         </h5>
         <div className="mt-4 border-t-2 border-zinc-200/60 pt-2">
@@ -16,7 +16,7 @@ export const Job = ({ data }) => {
       </div>
 
       <div className="mt-16 inline-flex items-center text-indigo-600">
-        <p className="font-medium md:text-lg">
+        <p className="font-medium lg:text-lg">
           {data.job_tenure} &#183; {data.job_type}
         </p>
       </div>
