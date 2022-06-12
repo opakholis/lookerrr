928561c9dde93348651da978ca65d1c7f6392bfb
Opa Kholis Majid <code@opakholis.space>
2022-06-13 01:02:32 +0700

    build: disable register route for a while

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './routes/home';
 import { Dashboard } from './routes/dashboard';
 import { Login } from './routes/login';
-import { Register } from './routes/register';
+// import { Register } from './routes/register';
 
 import { NotFound } from './routes/not-found';
 
@@ -13,7 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
+      {/* <Route path="register" element={<Register />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
