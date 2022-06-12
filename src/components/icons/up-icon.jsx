76de480d992e9cbbd0903f93aba1fa14382a1161
Opa Kholis Majid <code@opakholis.space>
2022-06-12 09:50:13 +0700

    fix: typo svg property

--- src/components/icons/up-icon.jsx
+++ src/components/icons/up-icon.jsx
@@ -7,7 +7,7 @@ export const ChevronUpIcon = () => {
       fill="currentColor"
     >
       <path
-        fillRrule="evenodd"
+        fillRule="evenodd"
         d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
         clipRule="evenodd"
       />
