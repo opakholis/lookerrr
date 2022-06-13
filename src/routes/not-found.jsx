7ee6f1eafd68150e5ec7c4e51f1ff4deaf8be952
Opa Kholis Majid <code@opakholis.space>
2022-06-13 14:14:28 +0700

    style: class to className

--- src/routes/not-found.jsx
+++ src/routes/not-found.jsx
@@ -9,14 +9,16 @@ export const NotFound = () => {
 
   return (
     <main className="mx-auto h-screen w-full max-w-screen-lg px-6 lg:px-12">
-      <div class="flex h-full w-full flex-col items-center justify-center">
-        <h2 class="text-lg text-zinc-600">Woppsss!</h2>
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <h2 className="text-lg text-zinc-600">Woppsss!</h2>
 
-        <p class="mt-4 text-2xl font-medium text-indigo-600">404 - Not Found</p>
+        <p className="mt-4 text-2xl font-medium text-indigo-600">
+          404 - Not Found
+        </p>
 
         <Link
           to="/"
-          class="mt-8 inline-flex items-center rounded-lg bg-indigo-600 px-5 py-3 font-medium text-white hover:bg-indigo-500"
+          className="mt-8 inline-flex items-center rounded-lg bg-indigo-600 px-5 py-3 font-medium text-white hover:bg-indigo-500"
         >
           Kembali
           <ArrowRightIcon className="ml-2 h-5 w-5 text-white" />
