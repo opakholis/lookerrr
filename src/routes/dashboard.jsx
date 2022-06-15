ff8b3a7866f396a3cdb17aaa2b710bddbe7206fb
Opa Kholis Majid <code@opakholis.space>
2022-06-15 11:51:47 +0700

    refactor: redirect to /login if access /dashboard without token

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,3 +1,18 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = 'Dashboard';
+  }, []);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (!token) navigate('/login');
+  }, []);
+
   return <div className="mx-auto h-full w-11/12">Dashboard</div>;
 };
