e944fc2e723be7f5a90a9facb159775e37851473
Opa Kholis Majid <code@opakholis.space>
2022-06-15 23:56:07 +0700

    style: simply syntax

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,9 @@ const getAllJobs = () => {
   return client.get('/job-vacancy');
 };
 
-const getJobById = (id) => {
-  return client.get(`/job-vacancy/${id}`);
-};
+const getJobById = (id) => client.get(`/job-vacancy/${id}`);
 
-const login = ({ email, password }) => {
-  return client.post('/login', { email, password });
-};
+const login = ({ email, password }) =>
+  client.post('/login', { email, password });
 
 export { getAllJobs, getJobById, login };
