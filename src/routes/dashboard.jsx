5b1952922aeba14be2766fcb475ed120920727b8
Opa Kholis Majid <code@opakholis.space>
2022-06-19 14:03:44 +0700

    refactor: add some statistics

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,9 +1,25 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { job } from '../api';
 
 import AuthContext from '../context/auth';
 
 export const Dashboard = () => {
   const { user } = useContext(AuthContext);
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      await job.all().then((res) => {
+        setData(res?.data.data);
+      });
+    } catch (err) {
+      console.log(err.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     document.title = 'Dashboard';
@@ -14,6 +30,23 @@ export const Dashboard = () => {
       <h1 className="text-lg font-medium text-zinc-800">
         Holaa, {user.name}! <span aria-hidden>👋</span>
       </h1>
+      <div className="mt-8">
+        <h2 className="py-3 font-medium tracking-wide text-zinc-800">
+          Statistik
+        </h2>
+        <div className="w-52 rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 shadow-xl">
+          <a className="block rounded-xl bg-white p-6 sm:p-8" href="">
+            <div className="mt-16 sm:pr-8">
+              <h5 className="text-xl font-bold text-gray-900">
+                {data?.length ?? '--'}
+              </h5>
+              <p className="mt-2 text-sm text-gray-500">
+                Total Lowongan Tersedia
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
