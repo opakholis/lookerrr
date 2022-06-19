583323b71b8a0c94b7716de9f948b89c1a74de78
Opa Kholis Majid <code@opakholis.space>
2022-06-19 13:28:59 +0700

    refactor: decrease height header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,7 +21,7 @@ export const Header = () => {
 
   return (
     <header className="border-b border-gray-100">
-      <div className="mx-auto flex h-20 max-w-screen-2xl items-center justify-between px-6 lg:px-12">
+      <div className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between px-6 lg:px-12">
         <div className="flex items-center">{renderLogo()}</div>
 
         <div className="flex items-center">
@@ -35,7 +35,7 @@ export const Header = () => {
           ) : (
             <Link
               to="/dashboard"
-              className="my-2.5 flex items-center rounded-xl py-2.5 pl-2.5 pr-4 hover:bg-zinc-100 md:space-x-2.5"
+              className="my-2.5 flex items-center rounded-xl p-2 hover:bg-zinc-100 md:space-x-2.5 md:py-1.5 md:pl-2.5 md:pr-4"
             >
               <img
                 src={user.image_url}
